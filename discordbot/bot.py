1d65349608f96ecd4810dd7d5feddd96ba57849a
Vivek Ashok <37447267+codephile1221@users.noreply.github.com>
2020-08-23 15:09:51 +0400

    Update on_message and shutdown

--- discordbot/bot.py
+++ discordbot/bot.py
@@ -16,47 +16,18 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-    
     if message.content.startswith('>'):
-        print('ignored quote')
         return
-    
-    if message.content.lower() == '!shutdown':
-        if message.author.name == 'EG Vivek':
-            print("paavi")
-            await message.channel.send("Ada paavi")
-            await bot.logout()
-        else:
-            await message.channel.send("!sHuTdOwN.......lmao nooooob")
-            #await message.channel.send(f"poda {message.author.name}")
- 
-    elif bot.user in message.mentions:
-        c_pan = bot.get_user(c_pandey)
-        await message.channel.send(f"X ae  badnaam hui, darling {c_pan.mention} liye... ")
-        
-    elif 'goodnight ujjwal' in message.content.lower():
-        for i in range(5):
-            await message.channel.send("goodnight ujjwal")
-        
-    elif 'like u' in message.content.lower() or 'like you' in message.content.lower():
-        await message.channel.send("WAAPAS RAK")
-
-    elif 'ded' in  message.content.lower()or 'dead' in message.content.lower():
-        await message.channel.send("https://tenor.com/bioro.gif")
-    
-    elif 'test' in message.content.lower() or 'exam' in message.content.lower():
-        await message.channel.send('MITOSISAAAAA????')
-    
-    elif 'what' in message.content.lower() and 'cs' in message.content.lower():
-        msg = choice(["Imblement da logik", "write a......BOOOOOOOOOLEAN EXPRESSION"])
-        await message.channel.send(msg)
-
-        
-#@bot.command(name = 'shutdown', help = 'shuts down the bot remotely')
-#async def shutdown(ctx):
-    #print('something')
-    #await ctx.send('ada paavi')
-    #await ctx.bot.logout()
-    #print("Bot has been shutdown by the command")
+    elif bot.user in message.mentions and ('are you x ae' in message.content.lower() or 'are you chulbul pandey' in message.content):
+        await message.channel.send("Nahi sir, woh to mera judavaan bhai he sir.\nWoh Shyam, meh Ram...")
+    elif message.content.lower() == 'ded':
+        await message.channel.send("<a:coffin_dance:737353384595685377>")
+    await bot.process_commands(message)
+       
+@bot.command(name = 'shutdown', help = 'shuts down the bot remotely',aliases = ['off','die','marana'])
+async def shutdown(ctx):
+    await ctx.channel.send("Understandable. Have a nice day ✌")
+    await ctx.bot.logout()
+    print(f"Bot has been shutdown by the command by {ctx.message.author.name}")
 
 bot.run(token)
