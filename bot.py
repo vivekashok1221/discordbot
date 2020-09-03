d7ab33706d830bb86417b1900de712b5ebc9cdb9
codephile1221 <vivekashok1221@gmail.com>
2020-09-03 20:13:31 +0400

    add Song class, title in message

--- bot.py
+++ bot.py
@@ -24,6 +24,13 @@ async def on_message(message):
         await message.delete()
     await bot.process_commands(message)
 
+class Song:
+    def __init__(self,url,stream_url,title,duration):
+        self.url = url
+        self.stream_url = stream_url
+        self.title = title
+        self.duration = duration        
+
 class Music(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -53,36 +60,41 @@ class Music(commands.Cog):
             else:
                 await ctx.channel.send('**Meh raazi, lekin Tu nahi raazi. `Please connect to a VC first.`**')
 
-    def next(self,ctx):
+    def playsong(self,ctx):
         if len(self.playlist) > 0:
-            self.url_ = self.playlist.pop(0)
-            asyncio.run_coroutine_threadsafe(self.play(ctx,self.url_),self.bot.loop)
-
+            self.currentsong = self.playlist.pop(0)
+            
+            before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"    
+            self.voice.play(discord.FFmpegPCMAudio(self.currentsong.stream_url,before_options=before_options),after =lambda e: self.playsong(ctx))
+            
+            asyncio.run_coroutine_threadsafe(ctx.channel.send(f"\U0001f4c0`Aapko Sunaana Chaahta Hoon:`\U0001f4c0\n{self.currentsong.url}"),self.bot.loop)
+    
     @commands.command()
     async def play(self,ctx,*args): #TODO: check if user is connected to VC
+        
+        self.searchquery = ' '.join(args)
 
-        def play_(url):
-            ydl_opts = {}
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                result = ydl.extract_info(url,download=False )
-                stream_url = result['formats'][0]['url'] 
-            before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"    
-            self.voice.play(discord.FFmpegPCMAudio(stream_url,before_options=before_options),after =lambda e: self.next(ctx))
-            
-        self.song = ' '.join(args)
-        YTresults = YoutubeSearch(self.song, max_results=1).to_dict()
+        YTresults = YoutubeSearch(self.searchquery, max_results=1).to_dict()
         for result in YTresults:
             url_ = 'https://www.youtube.com'+ result['url_suffix']
-        self.playlist.append(url_)
-
+        
+        ydl_opts = {}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            result = ydl.extract_info(url_,download=False )
+            stream_url = result['formats'][0]['url']
+            title = result['title'] 
+            duration = result['duration']
+            duration = f"{duration//60}:{duration%60}"
+        
         if self.voice.is_playing():
-            await ctx.channel.send('added to queue')
+            await ctx.channel.send(f'`{title}` has been added to queue')
+            songObj = Song(url_,stream_url,title,duration) 
+            self.playlist.append(songObj)
             return
-            #TODO: add to queue
         else:
-            self.url_ = self.playlist.pop(0)
-            play_(self.url_)
-            await ctx.channel.send(f"\U0001f4c0`Aapko Sunaana Chaahta Hoon:`\U0001f4c0\n{self.url_}")
+            songObj = Song(url_,stream_url,title,duration) # repeating is faster
+            self.playlist.append(songObj)
+            self.playsong(ctx)
             
     
 
@@ -107,17 +119,22 @@ class Music(commands.Cog):
 
     @commands.command(aliases = ['r'])
     async def remove (self,ctx,position : int):
-        if position < 0:
-            self.playlist.pop(position)
-        else:self.playlist.pop(position-1)
-        await ctx.channel.send("removed")
-    
+        if self.playlist:
+            if position < 0:
+                s =self.playlist.pop(position)
+            else:
+                s = self.playlist.pop(position-1)
+            await ctx.channel.send(f"removed `{s.title}` from the queue") #TODO: emoji
+            del s
     
     @commands.command(aliases = ['m'])
-    async def move(self,ctx,initial:int,final : int = 0):
+    async def move(self,ctx,initial:int,final : int = 1):
+        if initial < 1 or final < 1:
+            await ctx.channel.send("`Invalid Arguments, naughty boy`")
+            return
         s =self.playlist.pop(initial-1)
         self.playlist.insert(final-1,s)
-        await ctx.channel.send(f"moved to {final}")
+        await ctx.channel.send(f"`{s.title}` moved to position {final}")
         del s
 
     @commands.command(name = 'clear')
@@ -125,16 +142,19 @@ class Music(commands.Cog):
         self.playlist.clear()
         await ctx.channel.send("`Queue has been cleared`")
 
-    @commands.command(name = 'queue',aliases = ['q','ganas'])
+    @commands.command(name = 'queue',aliases = ['q','gaanas'])
     async def listqueue(self,ctx):
         plals = str(self.playlist)
         await ctx.channel.send(plals)
+        # embed = discord.Embed(title = 'Yeh hei tera queue:')
+        # embed.description = 'Now Playing: (some song)'
+
 
 
     @commands.command(help = f"Disconnects from Voice Channel", aliases = ['d'])
     async def disconnect(self,ctx):
         await self.voice.disconnect()
-        await ctx.channel.send('**Tumhari mansik isthithi aaj thodi gadbad lag rahi hai ... hum baad mein phir aayenge**\n`Disconnected from VC `')
+        await ctx.channel.send('**Tumhari mansik isthithi aaj thodi gadbad lag rahi hai ... hum baad mein phir aayenge**\n`Disconnected from VC`')
         self.playlist.clear()
 
 
