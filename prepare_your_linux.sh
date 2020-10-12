569f678880605cd195972beebbb05a52b4877a1c
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2020-10-12 10:34:04 -0300

    instalation to your linux Ubuntu like

---
+++ prepare_your_linux.sh
@@ -0,0 +1,7 @@
+sudo apt install -y python3
+sudo apt install -y python3-pip
+sudo pip install discord.py 
+sudo pip install PyNaCl
+sudo pip install python-dotenv
+sudo pip install youtube-dl
+sudo pip install youtube-search
