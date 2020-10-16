ac69c9e72a2e6cb338cf95a92820538458cb554b
codephile1221 <vivekashok1221@gmail.com>
2020-10-16 17:41:45 +0400

    move on_ready and on_command_error to events cog

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -7,34 +6,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
-
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user} has connected to discord...\n")
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        print("hmmmm")
-        return
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.channel.send("You are missing some arguments...")
-    elif isinstance(error, commands.BadArgument):
-        await ctx.channel.send("Try again. Bad arguments supplied.")
-    elif (cog := ctx.cog):
-        if cog.qualified_name == "Music" \
-                and isinstance(error, commands.CheckFailure):
-            return
-
-    bot_channel = discord.utils.get(ctx.guild.channels, name="bot-updates")
-    embed = discord.Embed(
-                        title="Uh-oh...",
-                        description=f"{type(error).__name__} : {error}",
-                        colour=discord.Colour.red()
-                        )
-    await bot_channel.send(f"{ctx.author.mention}", embed=embed)
 
 
 @bot.command()
--- cogs/events.py
+++ cogs/events.py
@@ -12,6 +12,32 @@ class Events(commands.Cog):
         await member.add_roles(role)
     # TODO: on ready uptime, error
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{self.bot.user} has connected to discord...\n")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            print("hmmmm")
+            return
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.channel.send("You are missing some arguments...")
+        elif isinstance(error, commands.BadArgument):
+            await ctx.channel.send("Try again. Bad arguments supplied.")
+        elif (cog := ctx.cog):
+            if cog.qualified_name == "Music" \
+                    and isinstance(error, commands.CheckFailure):
+                return
+
+        bot_channel = discord.utils.get(ctx.guild.channels, name="bot-updates")
+        embed = discord.Embed(
+                            title="Uh-oh...",
+                            description=f"{type(error).__name__} : {error}",
+                            colour=discord.Colour.red()
+                            )
+        await bot_channel.send(f"{ctx.author.mention}", embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Events(bot))
